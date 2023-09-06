ef2cd6a2d6b502e02ac238fd719ba2c0cd97c713
HeetOnGit <heetshah20012003@gmail.com>
2023-09-06 12:15:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import One from './One';
 function App() {
   return (
     <div>
-      <One />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h1>hello</h1>
+      {/* <One /> */}
+   
     </div>
   );
 }
